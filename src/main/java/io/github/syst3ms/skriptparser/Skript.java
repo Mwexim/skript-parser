400d5f5b24af7717f660b030d86c0eb3e04beff1
Syst3ms <arthmm14@gmail.com>
2018-07-08 11:44:42 +0200

    Script loading system
    We're getting close !

---
+++ src/main/java/io/github/syst3ms/skriptparser/Skript.java
@@ -0,0 +1,30 @@
+package io.github.syst3ms.skriptparser;
+
+import io.github.syst3ms.skriptparser.event.MainEvent;
+import io.github.syst3ms.skriptparser.lang.Effect;
+import io.github.syst3ms.skriptparser.lang.Trigger;
+import io.github.syst3ms.skriptparser.registration.SkriptAddon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Skript extends SkriptAddon {
+    private final String[] mainArgs;
+    private List<Trigger> mainTriggers = new ArrayList<>();
+
+    public Skript(String[] mainArgs) {
+        this.mainArgs = mainArgs;
+    }
+
+    @Override
+    public void handleTrigger(Trigger trigger) {
+        mainTriggers.add(trigger);
+    }
+
+    @Override
+    public void finishedLoading() {
+        for (Trigger trigger : mainTriggers) {
+            Effect.runAll(trigger, new MainEvent(mainArgs));
+        }
+    }
+}
