4b1d4d300833183bea45ea25848fbbd4ad09595e
unknown <alexlew95@gmail.com>
2020-07-08 20:51:11 +0200

    :sparkles: Added ExprDefaultValue

---
+++ src/main/java/io/github/syst3ms/skriptparser/expressions/ExprDefaultValue.java
@@ -0,0 +1,57 @@
+package io.github.syst3ms.skriptparser.expressions;
+
+import io.github.syst3ms.skriptparser.Main;
+import io.github.syst3ms.skriptparser.lang.TriggerContext;
+import io.github.syst3ms.skriptparser.lang.Expression;
+import io.github.syst3ms.skriptparser.parsing.ParseContext;
+
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A shorthand expression for giving things a default value. If the first thing isn't set, the second thing will be returned.
+ *
+ * @name Default Value
+ * @pattern %object% || %object%
+ * @pattern %object% (otherwise|?) %object%
+ * @since ALPHA
+ * @author Olyno
+ */
+public class ExprDefaultValue implements Expression<Object> {
+
+    private Expression<Object> firstValue, secondValue;
+
+    static {
+        Main.getMainRegistration().addExpression(
+            ExprDefaultValue.class,
+            Object.class,
+            false,
+            "%object% \\|\\| %object%",
+            "%object% (otherwise|?) %object%"
+        );
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
+        firstValue = (Expression<Object>) expressions[0];
+        secondValue = (Expression<Object>) expressions[1];
+        return true;
+    }
+
+    @Override
+    public Object[] getValues(TriggerContext ctx) {
+        Object first = firstValue.getSingle(ctx);
+        Object second = secondValue.getSingle(ctx);
+        if (first != null) {
+            return new Object[]{first};
+        } else {
+            return new Object[]{second};
+        }
+    }
+
+    @Override
+    public String toString(@Nullable TriggerContext ctx, boolean debug) {
+        return firstValue.toString(ctx, debug) + " otherwise " + secondValue.toString(ctx, debug);
+    }
+
+}
