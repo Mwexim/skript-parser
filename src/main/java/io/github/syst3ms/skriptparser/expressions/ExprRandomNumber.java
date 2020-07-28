fcb41b985c8f1c20b826ac3bea90dc30b8aa9f4a
WeeskyBDW <bibou.gamers@gmail.com>
2020-07-28 23:23:12 +0200

    :speech_balloon: Fix ExprRandomNumber toString return value

--- src/main/java/io/github/syst3ms/skriptparser/expressions/ExprRandomNumber.java
+++ src/main/java/io/github/syst3ms/skriptparser/expressions/ExprRandomNumber.java
@@ -5,42 +5,52 @@ import io.github.syst3ms.skriptparser.lang.Expression;
 import io.github.syst3ms.skriptparser.lang.TriggerContext;
 import io.github.syst3ms.skriptparser.parsing.ParseContext;
 import org.jetbrains.annotations.Nullable;
-
 import java.util.concurrent.ThreadLocalRandom;
-
+import java.lang.Math;
+/**
+ * Generate a random number (double) or integer.
+ *
+ * @name random number
+ * @pattern [a] random (integer|number) (from|between) %number% (to|and) %number%
+ * @since ALPHA
+ * @author WeeskyBDW
+ */
 public class ExprRandomNumber implements Expression<Number> {
 
     private Expression<Number> lowerNumber, maxNumber;
 
+	private boolean isInteger;
     static {
         Main.getMainRegistration().addExpression(
                 ExprRandomNumber.class,
                 Number.class,
                 true,
-                "test %number% to %number%"
+                "[a] random (1:integer|2:number) (from|between) %number% (to|and) %number%"
         );
     }
 
     @Override
     @SuppressWarnings("unchecked")
-    public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
-        System.out.println("yolo");
+    public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext context) {
         lowerNumber = (Expression<Number>) expressions[0];
         maxNumber = (Expression<Number>) expressions[1];
+		isInteger = context.getParseMark() == 1;
         return true;
     }
 
     @Override
     public Number[] getValues(TriggerContext ctx) {
-        System.out.println("test");
-        Number low = lowerNumber.getSingle(ctx);
-        Number max = maxNumber.getSingle(ctx).intValue();
-        System.out.println("test 2");
-        return new Integer[]{ThreadLocalRandom.current().nextInt(e, s + 1)};
+        Double low = lowerNumber.getSingle(ctx).doubleValue();
+        Double max = maxNumber.getSingle(ctx).doubleValue();
+		if(isInteger) {
+			return new Long[]{ThreadLocalRandom.current().nextLong(Math.round(low), Math.round(max) + 1)};
+		} else {
+			return new Double[]{ThreadLocalRandom.current().nextDouble(low, max + 1)};
+		}
     }
 
     @Override
     public String toString(@Nullable TriggerContext ctx, boolean debug) {
-        return "random number";
+        return "Generate random " + (isInteger ? "integer" : "number") + " from " + lowerNumber.toString() + " to " + maxNumber.toString();
     }
 }
