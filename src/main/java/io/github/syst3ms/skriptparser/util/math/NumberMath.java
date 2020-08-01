f38ab8583f94b942bb4aa53266f048c53256cd8a
Syst3ms <arthmm14@gmail.com>
2020-08-01 18:45:03 +0200

    Add randomness functions in NumberMath

--- src/main/java/io/github/syst3ms/skriptparser/util/math/NumberMath.java
+++ src/main/java/io/github/syst3ms/skriptparser/util/math/NumberMath.java
@@ -3,6 +3,7 @@ package io.github.syst3ms.skriptparser.util.math;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Utilities for various math operations using the generic {@link Number} type
@@ -13,6 +14,7 @@ import java.math.RoundingMode;
 public class NumberMath {
     private static final BigDecimal RADIANS_TO_DEGREES = new BigDecimal(180).divide(BigDecimalMath.pi(BigDecimalMath.DEFAULT_CONTEXT), BigDecimalMath.DEFAULT_CONTEXT);
     private static final BigDecimal DEGREES_TO_RADIANS = BigDecimalMath.pi(BigDecimalMath.DEFAULT_CONTEXT).divide(new BigDecimal(180), BigDecimalMath.DEFAULT_CONTEXT);
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
 
     public static Number abs(Number n) {
         if (n instanceof Long) {
@@ -183,6 +185,99 @@ public class NumberMath {
                                     .add(BigDecimalMath.exp(bigToBigDecimal(n).negate(), BigDecimalMath.DEFAULT_CONTEXT)),
                             BigDecimalMath.DEFAULT_CONTEXT
                     );
+        }
+    }
+
+    /**
+     * Computes a random number between the two given bounds
+     *
+     * The return type is related to the parameter types in the following way :
+     * <ul>
+     *     <li>Two bounds of the same type will return a result of the same type</li>
+     *     <li>If one of the bounds is a {@link BigDecimal}, so will be the result</li>
+     *     <li>If one of the bounds is a {@link BigInteger} and the other is a {@link Double}, the result will be a {@link BigDecimal}</li>
+     *     <li>If one of the bounds is a {@link BigInteger} and the other is a {@link Long}, the result will be a {@link BigInteger}</li>
+     *     <li>Otherwise (i.e one of the bounds is a {@code long} and the other a {@code double}), the result will be a {@code double}</li>
+     * </ul>
+     * @param lower the lower bound
+     * @param upper the upper bound
+     * @param inclusive whether the bounds are exclusive or not (skript-parser defaults to true in its implementation)
+     * @param random the {@link ThreadLocalRandom} instance used
+     * @return a random {@link Number} between the two given bounds
+     */
+    public static Number random(Number lower, Number upper, boolean inclusive, ThreadLocalRandom random) {
+        if (lower.equals(upper))
+            return lower;
+        if (inclusive) {
+            upper = changeBoundExclusion(upper);
+        } else {
+            lower = changeBoundExclusion(lower);
+        }
+        if (lower instanceof Long && upper instanceof Long) {
+            return random.nextLong((long) lower, (long) upper);
+        } else if (lower instanceof BigDecimal || upper instanceof BigDecimal) {
+            if (lower instanceof BigDecimal && upper instanceof BigDecimal) {
+                return randomBigDecimal((BigDecimal) lower, (BigDecimal) upper, random);
+            } else  {
+                return randomBigDecimal(BigDecimalMath.getBigDecimal(lower), BigDecimalMath.getBigDecimal(upper), random);
+            }
+        } else if (lower instanceof BigInteger || upper instanceof BigInteger) {
+            if (lower instanceof BigInteger && upper instanceof BigInteger) {
+                return randomBigInteger((BigInteger) lower, (BigInteger) upper, random);
+            } else if (lower instanceof Double || upper instanceof Double) {
+                return randomBigDecimal(BigDecimalMath.getBigDecimal(lower), BigDecimalMath.getBigDecimal(upper), random);
+            } else {
+                return randomBigInteger(BigDecimalMath.getBigInteger(lower), BigDecimalMath.getBigInteger(upper), random);
+            }
+        } else {
+            return random.nextDouble(lower.doubleValue(), upper.doubleValue());
+        }
+    }
+
+    /**
+     * @param lower the lower bound
+     * @param upper the upper bound
+     * @return a random {@link BigInteger} between {@code lower} (inclusive) and {@code upper} (exclusive)
+     */
+    public static BigInteger randomBigInteger(BigInteger lower, BigInteger upper, ThreadLocalRandom random) {
+        upper = upper.subtract(BigInteger.ONE); // Make the upper bound exclusive for consistency
+        BigInteger span = upper.subtract(lower);
+        int len = upper.bitLength();
+        BigInteger res = new BigInteger(len, random); // this creates a random BigInteger between 0 and the power of 2 above the upper bound
+        if (res.compareTo(lower) < 0 || res.compareTo(upper) > 0) {
+            /*
+             * If the result is not in the range we fit it into the span using modulo, and then add the lower bound.
+             */
+            res = res.mod(span).add(lower);
+        }
+        return res;
+    }
+
+    /**
+     * @param lower the lower bound
+     * @param upper the upper bound
+     * @return a random {@link BigDecimal} between {@code lower} (inclusive) and {@code upper} (exclusive)
+     */
+    public static BigDecimal randomBigDecimal(BigDecimal lower, BigDecimal upper, ThreadLocalRandom random) {
+        return lower.add(BigDecimal.valueOf(random.nextDouble()).multiply(upper.subtract(lower)));
+    }
+
+    private static Number changeBoundExclusion(Number n) {
+        /*
+         * The methods used for each number type have an inclusive lower bound but an exclusive upper bound.
+         * This method makes a lower bound exclusive and an upper bound inclusive.
+         */
+        if (n instanceof Long) {
+            return (long) n + 1L;
+        } else if (n instanceof BigInteger) {
+            return ((BigInteger) n).add(BigInteger.ONE);
+        } else if (n instanceof Double) {
+            double d = (double) n;
+            return d + Math.ulp(d);
+        } else {
+            BigDecimal bd = (BigDecimal) n;
+            BigDecimal nudge = BigDecimal.ONE.scaleByPowerOfTen(-BigDecimalMath.DEFAULT_CONTEXT.getPrecision());
+            return bd.add(nudge);
         }
     }
 
