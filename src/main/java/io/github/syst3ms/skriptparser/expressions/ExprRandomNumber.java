df3948eb17b7233b33aa5f88f93fd7799efb719d
WeeskyBDW <36575072+WeeskyBDW@users.noreply.github.com>
2020-08-03 22:32:26 +0200

    Add ExprRandomNumber (#18)
    Co-authored-by: WeeskyBDW <bibou.gamers@gmail.com>

---
+++ src/main/java/io/github/syst3ms/skriptparser/expressions/ExprRandomNumber.java
@@ -0,0 +1,67 @@
+package io.github.syst3ms.skriptparser.expressions;
+
+import io.github.syst3ms.skriptparser.Main;
+import io.github.syst3ms.skriptparser.lang.Expression;
+import io.github.syst3ms.skriptparser.lang.TriggerContext;
+import io.github.syst3ms.skriptparser.parsing.ParseContext;
+import io.github.syst3ms.skriptparser.types.comparisons.Comparator;
+import io.github.syst3ms.skriptparser.types.comparisons.Comparators;
+import io.github.syst3ms.skriptparser.types.comparisons.Relation;
+import io.github.syst3ms.skriptparser.util.math.NumberMath;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.ThreadLocalRandom;
+/**
+ * Generate a random number (double) or integer.
+ *
+ * @name random number
+ * @pattern [a] random integer [strictly] (from|between) %number% (to|and) %number%
+ * @pattern [a] random number [strictly] (from|between) %integer% (to|and) %integer%
+ * @since ALPHA
+ * @author WeeskyBDW
+ */
+public class ExprRandomNumber implements Expression<Number> {
+    private Expression<Number> lowerNumber, maxNumber;
+    private final ThreadLocalRandom random = ThreadLocalRandom.current();
+    private boolean isInteger, isExclusive;
+    private Comparator<? super Number, ? super Number> numComp;
+
+    static {
+        Main.getMainRegistration().addExpression(
+                ExprRandomNumber.class,
+                Number.class,
+                true,
+                "[a] random integer [(1:strictly)] (from|between) %integer% (to|and) %integer%",
+                "[a] random number [(1:strictly)] (from|between) %number% (to|and) %number%"
+        );
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext context) {
+        lowerNumber = (Expression<Number>) expressions[0];
+        maxNumber = (Expression<Number>) expressions[1];
+        isInteger = matchedPattern == 0;
+        isExclusive = context.getParseMark() == 1;
+        numComp = Comparators.getComparator(Number.class, Number.class);
+        assert numComp != null;
+        return true;
+    }
+
+    @Override
+    public Number[] getValues(TriggerContext ctx) {
+        Number low = lowerNumber.getSingle(ctx);
+        Number max = maxNumber.getSingle(ctx);
+        if (low == null || max == null)
+            return new Number[0];
+        //Check to find out which number is the greater of the 2, while keeping the type
+        Number realLow = numComp.apply(low, max).is(Relation.SMALLER_OR_EQUAL) ? low : max;
+        Number realMax = numComp.apply(low, max).is(Relation.SMALLER_OR_EQUAL) ? max : low;
+        return new Number[]{NumberMath.random(realLow, realMax, !isExclusive, random)};
+    }
+
+    @Override
+    public String toString(@Nullable TriggerContext ctx, boolean debug) {
+        return "a random " + (isInteger ? "integer " : "number ") + (isExclusive ? "strictly " : "") + "between " + lowerNumber.toString(ctx, debug) + " and " + maxNumber.toString(ctx, debug);
+    }
+}
