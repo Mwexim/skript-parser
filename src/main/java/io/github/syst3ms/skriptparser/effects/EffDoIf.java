5c35bc20acef015a4858948d29c928c739847520
CHarcoalToast <CharcoalToast@users.noreply.github.com>
2020-08-21 19:15:29 +0200

    [Fix/Feat.] Added EffDoIf and fixed issue with EffAsync throwing UnsupportedOperationException

--- src/main/java/io/github/syst3ms/skriptparser/effects/EffDoIf.java
+++ src/main/java/io/github/syst3ms/skriptparser/effects/EffDoIf.java
@@ -4,36 +4,36 @@ import io.github.syst3ms.skriptparser.Main;
 import io.github.syst3ms.skriptparser.lang.Effect;
 import io.github.syst3ms.skriptparser.lang.Expression;
 import io.github.syst3ms.skriptparser.lang.TriggerContext;
+import io.github.syst3ms.skriptparser.lang.base.ConditionalExpression;
 import io.github.syst3ms.skriptparser.parsing.ParseContext;
 import io.github.syst3ms.skriptparser.parsing.SyntaxParser;
 import io.github.syst3ms.skriptparser.util.ThreadUtils;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Runs this effect asynchronously.
- * The difference between this and the section {@code async} is due to the fact that this effect will only run the specified action asunc,
- * while the section runs all the effects under the statement async.
- * If you only want to do one operation async, this is the effect you want to go with.
+ * Runs this effect if a given condition succeeds.
  *
- * @name Async
+ * @name Do If
  * @type EFFECT
- * @pattern async[hronous[ly]] <.+>
+ * @pattern [do] <.+> [only] if %=boolean%
  * @since ALPHA
  * @author Mwexim
  */
-public class EffAsync extends Effect {
+public class EffDoIf extends Effect {
 
     static {
         Main.getMainRegistration().addEffect(
-            EffAsync.class,
-            "async[hronous[ly]] [do] <.+>"
+            EffDoIf.class,
+            "[do] <.+> [only] if %=boolean%"
         );
     }
 
+    ConditionalExpression condition;
     private Effect effect;
 
     @Override
     public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
+        condition = (ConditionalExpression) expressions[0];
         String expr = parseContext.getMatches().get(0).group();
         parseContext.getLogger().recurse();
         effect = SyntaxParser.parseEffect(expr, parseContext.getParserState(), parseContext.getLogger());
@@ -43,11 +43,12 @@ public class EffAsync extends Effect {
 
     @Override
     public void execute(TriggerContext ctx) {
-        ThreadUtils.runAsync(() -> effect.walk(ctx));
+        if (condition.check(ctx))
+            effect.walk(ctx);
     }
 
     @Override
     public String toString(@Nullable TriggerContext ctx, boolean debug) {
-        return "async " + effect.toString(ctx, debug);
+        return effect.toString(ctx, debug) + " if " + condition.toString(ctx, debug);
     }
 }
