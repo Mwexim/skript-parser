b648ef1ce320eb04e0b23f6fd18822b3fcc1d0ee
Olyno <olyno.dev@gmail.com>
2020-09-03 04:44:09 +0200

    :sparkles: Added more expressions

--- src/main/java/io/github/syst3ms/skriptparser/expressions/ExprMutableList.java
+++ src/main/java/io/github/syst3ms/skriptparser/expressions/ExprMutableList.java
@@ -11,42 +11,63 @@ import io.github.syst3ms.skriptparser.lang.TriggerContext;
 import io.github.syst3ms.skriptparser.parsing.ParseContext;
 
 /**
- * Reverses given list.
+ * Reverses, shuffle or order given list.
  *
- * @name Reversed List
- * @pattern reversed %objects% 
+ * @name Mutable List
+ * @pattern reverse[d] %objects%
+ * @pattern shuffle[d] %objects%
+ * @pattern sort[ed] %objects%
  * @since ALPHA
  * @author Olyno
  */
-public class ExprReversedList implements Expression<Object> {
+public class ExprMutableList implements Expression<Object> {
+
+    private enum MutableType {
+        REVERSE, SHUFFLE, SORT
+    }
 
     static {
         Parser.getMainRegistration().addExpression(
-            ExprReversedList.class,
+            ExprMutableList.class,
             Object.class,
             false,
-            "reversed %objects%"
+            "reverse[d] %objects%",
+            "shuffle[d] %objects%",
+            "sort[ed] %objects%"
         );
     }
 
     private Expression<Object> list;
+    private MutableType type;
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
         list = (Expression<Object>) expressions[0];
+        type = MutableType.values()[matchedPattern];
         return true;
     }
 
     @Override
     public Object[] getValues(TriggerContext ctx) {
         Object[] values = list.getValues(ctx);
-        Collections.reverse(Arrays.asList(values)); 
+        switch (type) {
+            case REVERSE:
+                Collections.reverse(Arrays.asList(values));
+                break;
+            case SHUFFLE:
+                Collections.shuffle(Arrays.asList(values));
+                break;
+            case SORT:
+                Arrays.sort(values);
+                break;
+
+        }
         return values;
     }
 
     @Override
     public String toString(@Nullable TriggerContext ctx, boolean debug) {
-        return "reversed " + list.toString(ctx, debug);
+        return type.name().replaceAll("_", "").toLowerCase() + " " + list.toString(ctx, debug);
     }
 }
