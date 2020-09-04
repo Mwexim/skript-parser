f4fd36b5811df9cce2d712fecfb395c5170de82e
CHarcoalToast <CharcoalToast@users.noreply.github.com>
2020-09-04 21:19:43 +0200

    [Feature] Add EffShutdown, EffCompose, EffEscape

---
+++ src/main/java/io/github/syst3ms/skriptparser/effects/EffEscape.java
@@ -0,0 +1,70 @@
+package io.github.syst3ms.skriptparser.effects;
+
+import io.github.syst3ms.skriptparser.Parser;
+import io.github.syst3ms.skriptparser.lang.Effect;
+import io.github.syst3ms.skriptparser.lang.Expression;
+import io.github.syst3ms.skriptparser.lang.Statement;
+import io.github.syst3ms.skriptparser.lang.TriggerContext;
+import io.github.syst3ms.skriptparser.parsing.ParseContext;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+
+/**
+ * Skips a given amount of lines and runs the code after.
+ * Note that lines that have more indentation than this one are not considered in the amount.
+ * Nevertheless, lines that have less indentation will be considered.
+ *
+ * @name Escape
+ * @pattern escape %integer% [(level[s]|line[s])]
+ * @since ALPHA
+ * @author Mwexim
+ */
+public class EffEscape extends Effect {
+
+    static {
+        Parser.getMainRegistration().addEffect(
+            EffEscape.class,
+            "escape %integer% [(level[s]|line[s])]"
+        );
+    }
+
+    private Expression<Long> amount;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
+        amount = (Expression<Long>) expressions[0];
+        return true;
+    }
+
+    @Override
+    public void execute(TriggerContext ctx) {
+        throw new UnsupportedOperationException();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Optional<? extends Statement> walk(TriggerContext ctx) {
+        if (amount.getSingle(ctx).isEmpty())
+            return getNext();
+        long am = amount.getSingle(ctx).get();
+
+        Optional<Statement> current = Optional.of(this);
+        while (am > 0) {
+            if (current.isEmpty()) {
+                return Optional.empty();
+            }
+            current = (Optional<Statement>) current.get().getNext();
+            am--;
+        }
+        if (current.isEmpty())
+            return Optional.empty();
+        return current.get().getNext();
+    }
+
+    @Override
+    public String toString(@Nullable TriggerContext ctx, boolean debug) {
+        return "escape " + amount.toString(ctx, debug) + " lines";
+    }
+}
