f6125b5e92e516c4cc71fc7ddf08d04388841588
CHarcoalToast <CharcoalToast@users.noreply.github.com>
2020-09-05 17:14:46 +0200

    [Feature] Add ExprDurationSince and ExprDateFormatted

---
+++ src/main/java/io/github/syst3ms/skriptparser/expressions/ExprDateFormatted.java
@@ -0,0 +1,55 @@
+package io.github.syst3ms.skriptparser.expressions;
+
+import io.github.syst3ms.skriptparser.Parser;
+import io.github.syst3ms.skriptparser.lang.Expression;
+import io.github.syst3ms.skriptparser.lang.TriggerContext;
+import io.github.syst3ms.skriptparser.parsing.ParseContext;
+import io.github.syst3ms.skriptparser.util.DoubleOptional;
+import io.github.syst3ms.skriptparser.util.SkriptDate;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Formats a date as a string using the given format.
+ * Learn <a href="https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html">here</a>
+ * how you can use different formats.
+ *
+ * @name Formatted Date
+ * @type EXPRESSION
+ * @pattern [date] %date% formatted as %string%
+ * @since ALPHA
+ * @author Mwexim
+ */
+public class ExprDateFormatted implements Expression<String> {
+
+	static {
+		Parser.getMainRegistration().addExpression(
+				ExprDateFormatted.class,
+				String.class,
+				true,
+				"[date] %date% formatted as %string%"
+		);
+	}
+
+	Expression<SkriptDate> date;
+	Expression<String> format;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
+		date = (Expression<SkriptDate>) expressions[0];
+		format = (Expression<String>) expressions[1];
+		return true;
+	}
+
+	@Override
+	public String[] getValues(TriggerContext ctx) {
+		return DoubleOptional.ofOptional(date.getSingle(ctx), format.getSingle(ctx))
+				.mapToOptional((da, f) -> new String[] {da.toString(f)})
+				.orElse(new String[0]);
+	}
+
+	@Override
+	public String toString(@Nullable TriggerContext ctx, boolean debug) {
+		return date.toString(ctx, debug) + "formatted as " + format.toString(ctx, debug);
+	}
+}
