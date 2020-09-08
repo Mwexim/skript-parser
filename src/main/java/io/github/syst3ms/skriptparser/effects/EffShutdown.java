b6e3628ee5aac0bae73054471b69f56e38a75e5b
Mwexim <38397639+Mwexim@users.noreply.github.com>
2020-09-08 17:04:05 +0200

    [Feature] Add EffShutdown and EffEscape (#63)
    * [Feature] Add EffShutdown, EffCompose, EffEscape
    
    * Update src/main/java/io/github/syst3ms/skriptparser/effects/EffEscape.java
    
    Co-authored-by: Olyno <olyno.dev@gmail.com>
    
    * [Req.] Remove EffCompose
    
    * [Request] Add requests
    
    Co-authored-by: CHarcoalToast <CharcoalToast@users.noreply.github.com>
    Co-authored-by: Olyno <olyno.dev@gmail.com>

---
+++ src/main/java/io/github/syst3ms/skriptparser/effects/EffShutdown.java
@@ -0,0 +1,44 @@
+package io.github.syst3ms.skriptparser.effects;
+
+import io.github.syst3ms.skriptparser.Parser;
+import io.github.syst3ms.skriptparser.lang.Effect;
+import io.github.syst3ms.skriptparser.lang.Expression;
+import io.github.syst3ms.skriptparser.lang.TriggerContext;
+import io.github.syst3ms.skriptparser.parsing.ParseContext;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Shuts down the whole current sessions.
+ * This means that any code under any circumstances will not be run after this effect is triggered.
+ * Note that you should use this effect very cautiously.
+ *
+ * @name Shutdown
+ * @pattern shut[ ]down [[the] current session]
+ * @since ALPHA
+ * @author Mwexim
+ */
+public class EffShutdown extends Effect {
+    private Expression<String> string;
+
+    static {
+        Parser.getMainRegistration().addEffect(
+            EffShutdown.class,
+            "shut[ ]down [[the] current session]"
+        );
+    }
+
+    @Override
+    public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
+        return true;
+    }
+
+    @Override
+    public void execute(TriggerContext ctx) {
+        System.exit(0);
+    }
+
+    @Override
+    public String toString(@Nullable TriggerContext ctx, boolean debug) {
+        return "shutdown";
+    }
+}
