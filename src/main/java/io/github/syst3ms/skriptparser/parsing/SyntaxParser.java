8baec1ca977a7dc8048c6ab04bd4eadefbd21f12
Mwexim <38397639+Mwexim@users.noreply.github.com>
2020-10-25 12:38:34 +0100

    improve: small code improvement

--- src/main/java/io/github/syst3ms/skriptparser/parsing/SyntaxParser.java
+++ src/main/java/io/github/syst3ms/skriptparser/parsing/SyntaxParser.java
@@ -121,9 +121,8 @@ public class SyntaxParser {
                 return variable;
             }
         }
-        // The isList variable's sole purpose is to prevent us from exchanging boolean operators with lists.
-        boolean isList = s.toLowerCase().startsWith("list ");
-        if (isList) {
+        // This is to prevent us from exchanging boolean operators with lists.
+        if (s.toLowerCase().startsWith("list ")) {
             s = s.substring("list ".length());
         } else {
             // We parse boolean operators first to prevent clutter while parsing.
