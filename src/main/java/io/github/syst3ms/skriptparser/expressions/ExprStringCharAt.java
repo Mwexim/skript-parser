469cdd4dd6b6be0d9fa31fb0cd6d422541cca214
CHarcoalToast <CharcoalToast@users.noreply.github.com>
2020-10-25 15:47:46 +0100

    request: add requests

--- src/main/java/io/github/syst3ms/skriptparser/expressions/ExprStringCharAt.java
+++ src/main/java/io/github/syst3ms/skriptparser/expressions/ExprStringCharAt.java
@@ -1,24 +1,21 @@
 package io.github.syst3ms.skriptparser.expressions;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import io.github.syst3ms.skriptparser.Parser;
 import io.github.syst3ms.skriptparser.lang.Expression;
 import io.github.syst3ms.skriptparser.lang.TriggerContext;
 import io.github.syst3ms.skriptparser.parsing.ParseContext;
 import io.github.syst3ms.skriptparser.util.DoubleOptional;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * The character at a given position in a string. Note that indices in Skript start at 1.
  *
- * @name Character at
+ * @name Character At
  * @pattern char[acter] at [(index|pos[ition])] %number% (of|in) %string%
  * @since ALPHA
  * @author Olyno
  */
 public class ExprStringCharAt implements Expression<String> {
-
 	static {
 		Parser.getMainRegistration().addExpression(
 			ExprStringCharAt.class,
@@ -33,22 +30,22 @@ public class ExprStringCharAt implements Expression<String> {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public boolean init(final Expression<?> @NotNull [] expressions, final int matchedPattern, @NotNull final ParseContext parseContext) {
+	public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
 		position = (Expression<Integer>) expressions[0];
 		value = (Expression<String>) expressions[1];
 		return true;
 	}
 
 	@Override
-	public String[] getValues(final TriggerContext ctx) {
+	public String[] getValues(TriggerContext ctx) {
 		return DoubleOptional.ofOptional(value.getSingle(ctx), position.getSingle(ctx))
 			.filter((val, pos) -> pos > 0 && pos <= val.length())
-			.mapToOptional((val, pos) -> new String[]{ String.valueOf(val.charAt(pos - 1)) } )
+			.mapToOptional((val, pos) -> new String[] {String.valueOf(val.charAt(pos - 1))})
 			.orElse(new String[0]);
 	}
 
 	@Override
-	public String toString(@Nullable final TriggerContext ctx, final boolean debug) {
-		return "character at " + position.toString(ctx, debug) + " in " + value.toString(ctx, debug);
+	public String toString(@Nullable TriggerContext ctx, boolean debug) {
+		return "character at index " + position.toString(ctx, debug) + " in " + value.toString(ctx, debug);
 	}
 }
