1354edf2f3760b870782a4280e0d06b190caa8d5
Olyno <olyno.dev@gmail.com>
2020-10-31 19:17:32 +0100

    [feature] Added ExprMutableList (#62)
    * :sparkles: Added ExprReversedList
    
    * :sparkles: Added more expressions
    
    * request: add requests

---
+++ src/main/java/io/github/syst3ms/skriptparser/expressions/ExprMutableList.java
@@ -0,0 +1,72 @@
+package io.github.syst3ms.skriptparser.expressions;
+
+import io.github.syst3ms.skriptparser.Parser;
+import io.github.syst3ms.skriptparser.lang.Expression;
+import io.github.syst3ms.skriptparser.lang.TriggerContext;
+import io.github.syst3ms.skriptparser.parsing.ParseContext;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Reverse, shuffle or order a given list.
+ *
+ * @name Mutable List
+ * @pattern reverse[d] %objects%
+ * @pattern shuffle[d] %objects%
+ * @pattern sort[ed] %objects%
+ * @since ALPHA
+ * @author Olyno
+ */
+public class ExprMutableList implements Expression<Object> {
+    static {
+        Parser.getMainRegistration().addExpression(
+            ExprMutableList.class,
+            Object.class,
+            false,
+            "reverse[d] %objects%",
+            "shuffle[d] %objects%",
+            "sort[ed] %objects%"
+        );
+    }
+
+    private Expression<Object> list;
+    private int type;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
+        list = (Expression<Object>) expressions[0];
+        type = matchedPattern;
+        return true;
+    }
+
+    @Override
+    public Object[] getValues(TriggerContext ctx) {
+        Object[] values = list.getValues(ctx);
+        switch (type) {
+            case 0:
+                Collections.reverse(Arrays.asList(values));
+                break;
+            case 1:
+                Collections.shuffle(Arrays.asList(values));
+                break;
+            case 2:
+                Arrays.sort(values);
+                break;
+            default:
+                throw new IllegalStateException();
+        }
+        return values;
+    }
+
+    @Override
+    public String toString(@Nullable TriggerContext ctx, boolean debug) {
+        return (type == 0
+                ? "reversed " : type == 1
+                ? "shuffled "
+                : "sorted ")
+                + list.toString(ctx, debug);
+    }
+}
