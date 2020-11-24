f13da44b14893fd5a4b9d97d94eb82391b4f3086
Mwexim <CharcoalToast@users.noreply.github.com>
2020-11-24 11:24:21 +0100

    improve: sort tests in containers

--- src/test/java/io/github/syst3ms/skriptparser/parsing/SyntaxParserTest.java
+++ src/test/java/io/github/syst3ms/skriptparser/parsing/SyntaxParserTest.java
@@ -4,6 +4,8 @@ import io.github.syst3ms.skriptparser.TestRegistration;
 import io.github.syst3ms.skriptparser.log.LogType;
 import io.github.syst3ms.skriptparser.registration.SkriptAddon;
 import io.github.syst3ms.skriptparser.variables.Variables;
+import org.junit.jupiter.api.DynamicContainer;
+import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.runners.model.MultipleFailureException;
@@ -20,37 +22,43 @@ public class SyntaxParserTest {
     private static final List<Throwable> errorsFound = new ArrayList<>();
 
     @TestFactory
-    public Iterator<DynamicTest> syntaxTest() {
+    public Iterator<DynamicNode> syntaxTest() {
         String[] folders = {"effects", "expressions", "literals", "sections", "tags"};
-        ArrayList<DynamicTest> testsList = new ArrayList<>();
+        ArrayList<DynamicNode> containerList = new ArrayList<>();
         for (String folder : folders) {
+            ArrayList<DynamicTest> testsList = new ArrayList<>();
             for (File file : getResourceFolderFiles(folder)) {
                 if (file.getName().startsWith("-"))
                     continue;
                 testsList.add(
-                    DynamicTest.dynamicTest("Testing '" + file.getName().replaceAll("\\..+$", "") + "'", () -> {
-                        var logs = ScriptLoader.loadScript(file.toPath(), false);
-                        logs.removeIf(log -> log.getType() != LogType.ERROR);
-                        if (!logs.isEmpty()) {
-                            logs.forEach(log -> errorsFound.add(new SkriptParserException(log.getMessage())));
-                        }
-                        SkriptAddon.getAddons().forEach(SkriptAddon::finishedLoading);
+                        DynamicTest.dynamicTest("Testing '" + file.getName().replaceAll("\\..+$", "") + "'", () -> {
+                            var logs = ScriptLoader.loadScript(file.toPath(), false);
+                            logs.removeIf(log -> log.getType() != LogType.ERROR);
+                            if (!logs.isEmpty()) {
+                                logs.forEach(log -> errorsFound.add(new SkriptParserException(log.getMessage())));
+                            }
+                            SkriptAddon.getAddons().forEach(SkriptAddon::finishedLoading);
 
-                        // For some weird reason some errors are duplicated
-                        var allErrors = new ArrayList<>(errorsFound);
-                        Set<String> duplicateErrors = new HashSet<>();
-                        allErrors.removeIf(val -> !duplicateErrors.add(val.getMessage()));
+                            // For some weird reason some errors are duplicated
+                            var allErrors = new ArrayList<>(errorsFound);
+                            Set<String> duplicateErrors = new HashSet<>();
+                            allErrors.removeIf(val -> !duplicateErrors.add(val.getMessage()));
 
-                        // Reset variables
-                        Variables.clearVariables();
-                        errorsFound.clear();
+                            // Reset variables
+                            Variables.clearVariables();
+                            errorsFound.clear();
 
-                        MultipleFailureException.assertEmpty(allErrors);
-                    })
+                            MultipleFailureException.assertEmpty(allErrors);
+                        })
                 );
             }
+
+            containerList.add(DynamicContainer.dynamicContainer(
+                    folder,
+                    testsList
+            ));
         }
-        return testsList.iterator();
+        return containerList.iterator();
     }
 
     public static void addError(Throwable err) {
