36d26679895fe980244d78d173dda1b05eab1488
LimeGlass <16087552+TheLimeGlass@users.noreply.github.com>
2023-10-27 09:56:48 -0600

    Update src/test/java/io/github/syst3ms/skriptparser/parsing/SyntaxParserTest.java
    Co-authored-by: Mwexim <38397639+Mwexim@users.noreply.github.com>

--- src/test/java/io/github/syst3ms/skriptparser/parsing/SyntaxParserTest.java
+++ src/test/java/io/github/syst3ms/skriptparser/parsing/SyntaxParserTest.java
@@ -101,7 +101,7 @@ public class SyntaxParserTest {
         URL url = ClassLoader.getSystemResource(folder);
         if (url != null) {
             var path = url.getPath();
-            File[] files = new File(path).listFiles();
+            var files = new File(path).listFiles();
             if (files != null)
             	return files;
         }
