43275d015a8beb5ead2b0d260fc654e0aecc0ef0
TheLimeGlass <seantgrover@gmail.com>
2024-03-27 13:22:19 -0600

    Re-add the clear variables but package access only

--- src/test/java/io/github/syst3ms/skriptparser/variables/VariablesTest.java
+++ src/test/java/io/github/syst3ms/skriptparser/variables/VariablesTest.java
@@ -7,7 +7,7 @@ import io.github.syst3ms.skriptparser.log.SkriptLogger;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +26,7 @@ public class VariablesTest {
         Variables.registerStorage(RamStorage.class, "ram");
     }
 
-    @BeforeAll
+    @BeforeEach
     public void clearVariables() {
         Variables.clearVariables();
     }
